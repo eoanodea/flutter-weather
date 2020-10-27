5656923341b85f9dec444d0d71762107a7c64b34
Eoan O Dea <odeaeoan@gmail.com>
2020-10-27 12:46:36 +0100

    Added http package, http get request and random colour select on homepage because I'm bored waiting for the tutorial to get somewhere

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_weather/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -13,6 +16,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   void getLocation() async {
@@ -21,21 +25,44 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      String data = response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  List<Color> colours = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.pink,
+    Colors.teal,
+    Colors.black,
+    Colors.orange
+  ];
+
+  Color randomColour() {
+    final _random = new Random();
+    return colours[_random.nextInt(colours.length)];
+  }
+
+  Color selectedColour = Colors.red;
+
   @override
   Widget build(BuildContext context) {
-    String myMargin = 'abc';
-    double myMarginAsDouble;
-    try {
-      myMarginAsDouble = double.parse(myMargin);
-    } catch (e) {
-      print(e);
-    }
-
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        margin: EdgeInsets.all(
-          myMarginAsDouble ?? 30.0,
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedColour = randomColour();
+          });
+        },
+        child: Container(
+          color: selectedColour,
         ),
       ),
     );
